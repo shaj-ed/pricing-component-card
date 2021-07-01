c338f0e0bc82c45c56f73e6ff3d42bf4ce21498c
shajedd <shajedsr@gmial.com>
2021-07-01 19:23:43 +0600

    commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Frontend Mentor Solution - Pricing component with toggle
+
+![Design preview for the Pricing component with toggle coding challenge](./images/desktop-preview.jpg)
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Control the toggle with both their mouse/trackpad and their keyboard
+
+## Solution URL
+
+- [Frontend Mentor]()
+- [Netlify]()
---
+++ app.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+  initFuntion();
+});
+
+function initFuntion() {
+  const toggleButton = document.getElementById("toggle-button");
+  const circle = document.getElementById("circle");
+
+  toggleButton.addEventListener("click", (e) => {
+    circle.classList.toggle("tog");
+
+    priceToggle();
+  });
+}
+
+function priceToggle() {
+  const pricesYear = document.querySelectorAll("#price-year");
+  const pricesMonth = document.querySelectorAll("#price-month");
+
+  pricesMonth.forEach((element) => element.classList.toggle("hidden"));
+  pricesYear.forEach((element) => element.classList.toggle("hidden"));
+}
